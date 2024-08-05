2700518e28790d41660c832c707c04e75d32de43
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-08-06 00:25:24 +0300

    Server status

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Index views"""
+
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """A function return the status of the server"""
+    app_status = {"status": "OK"}
+    return jsonify(app_status)
