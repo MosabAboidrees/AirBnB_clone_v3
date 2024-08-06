0ccc0a80623a267aa153f3c18e96e35e15b94a53
MosabAboidrees <mosab_mahala@yahoo.com>
2024-08-05 18:41:08 -0700

    index fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 """Index views"""
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 from models import storage
 from flask import jsonify
+from flask import Flask
 from api.v1.views import app_views
 
 
@@ -20,10 +15,9 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def count():
     """An endpoint that retrieves the number of each objects by type"""
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-    classes = [Amenity, City, Place, Review, State, User]
-
-    object_count = {}
-    for i in range(len(classes)):
-        object_count[names[i]] = storage.count(classes[i])
-    return jsonify(object_count)
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
